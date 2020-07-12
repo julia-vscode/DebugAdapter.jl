aa22e55b88b243a8cbcfdddf9ee15cf128bac8dd
David Anthoff <anthoff@berkeley.edu>
2020-07-11 20:10:04 -0700

    Handle error situation when trying to debug a non existent file

--- src/debugger_utils.jl
+++ src/debugger_utils.jl
@@ -1,7 +1,3 @@
-function _parse_julia_file(filename::String)
-    return Base.parse_input_line(read(filename, String); filename = filename)
-end
-
 function lowercase_drive(a)
     if length(a) >= 2 && a[2] == ':'
         return lowercase(a[1]) * a[2:end]

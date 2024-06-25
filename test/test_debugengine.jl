36856c9e6689b3b69ddd4fdf57d047489ea2b48a
David Anthoff <anthoff@berkeley.edu>
2024-06-25 14:40:48 +0200

    Add a real test

--- test/test_debugengine.jl
+++ test/test_debugengine.jl
@@ -1,14 +1,19 @@
 @testitem "Run some code" begin
     import DebugAdapter.DebugEngines
-    using .DebugEngines: DebugEngine
 
-    de = DebugEngine(
-        Main,
-        "println(\"Hello world\")",
+    module TestValue
+        x = "Code didn't run"
+    end
+
+    de = DebugEngines.DebugEngine(
+        TestValue,
+        "x = true",
         "foo.jl",
         false,
         a -> println("a called")
     )
 
     run(de)
+
+    @test TestValue.x == true
 end
